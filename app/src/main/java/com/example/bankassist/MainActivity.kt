dae10dd681f8f5a536c7b3f79c1c4108ecf33c19
Rishabh Khurana <rishabhk994@gmail.com>
2020-03-27 23:36:32 +1100

    added view access variables and changed text size

--- app/src/main/java/com/example/bankassist/MainActivity.kt
+++ app/src/main/java/com/example/bankassist/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.bankassist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // customer number from database
+        val cutomerNumber = 123456
+        // login button from login Page
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        // customer number entered by user
+        val customerInput = findViewById<View>(R.id.customerInput)
     }
 }
