98a246d47667475d3cbb46ef16643e50abe73b20
Rishabh Khurana <rishabhk994@gmail.com>
2020-03-28 00:30:43 +1100

    password func added

--- app/src/main/java/com/example/bankassist/MainActivity.kt
+++ app/src/main/java/com/example/bankassist/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.bankassist
-
+// TODO:install a native database
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -11,16 +11,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // customer number from database
-        val cutomerNumber = 123456
+        // customer number from database for testing
+        var cutomerNumber = 123456
         // login button from login Page
         val loginButton = findViewById<Button>(R.id.loginButton)
         // customer number entered by user
         val customerInput = findViewById<View>(R.id.customerInput)
+        // password entered by user
+        val customerPass = findViewById<View>(R.id.customerInput)
 
-        loginButton.setOnClickListener {
-            // TODO : check if customer ID exists in database
-            // TODO : if not show error message
-        }
+//        loginButton.setOnClickListener {
+//            // TODO : check if customer ID exists in database and then password
+//            // TODO : if password or ID does not exist show error messaage
+//            // TODO : if all is correct naviaget to bank services view
+//        }
     }
 }
